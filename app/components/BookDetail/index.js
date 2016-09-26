65b957ad780ec0e35cf9b3fcafeaa53bbc792fc9
lakeoffaith <905663280@qq.com>
2016-09-26 17:34:56 +0800

    edit book detail

--- app/components/BookDetail/index.js
+++ app/components/BookDetail/index.js
@@ -5,6 +5,8 @@ import {
   Image
 } from 'react-native';
 import styles from './styles.js';
+import {Icon} from 'react-native-material-design';
+import {PrimaryColor,Accent,PrimaryText,SecondText,DividerText} from '../ijoyComponents/color'
 export default class BookDetail extends Component{
   render(){
     return(
@@ -12,9 +14,21 @@ export default class BookDetail extends Component{
           <View style={styles.imageContainer}>
               <Image source={require('../../img/book.jpg')} style={styles.image}/>
               <View style={styles.tipContainer}>
-                  <View style={styles.tipItemContainer}><Text>评分的区域</Text></View>
-                  <View style={styles.tipItemContainer}><Text>已经出售的区域</Text></View>
+                  <View style={styles.tipItemContainer}><Text>9.8分/1000阅读量</Text></View>
               </View>
+          </View>
+
+          <View style={styles.titleContainer}>
+             <Text style={{fontSize:18}}>Presto 技术内幕</Text>
+          </View>
+          <View style={styles.priceContainer}>
+              <Icon name="attach-money" size={16}/>
+             <Text style={{color:Accent}}>48.00</Text>
+          </View>
+          <View style={styles.picdocContainer}>
+            <View style={{width:40}}><Icon name="attachment" size={16}/></View>
+            <View style={{flex:1}}><Text>查看图文详情</Text></View>
+            <View style={{width:40,justifyContent:'center'}}><Icon name="keyboard-arrow-right" size={16}/></View>
           </View>
       </View>
     );
--- app/components/BookDetail/styles.js
+++ app/components/BookDetail/styles.js
@@ -1,4 +1,4 @@
-import { StyleSheet } from 'react-native';
+import { StyleSheet,Dimensions } from 'react-native';
 
 export default StyleSheet.create({
 	imageContainer: {
@@ -7,24 +7,45 @@ export default StyleSheet.create({
     justifyContent:'center'
 	},
   image:{
-    width:200,
-    height:200
+    width:Dimensions.get('window').width,
+    height:240
   },
   tipContainer:{
     position:'absolute',
     left:0,
     bottom:0,
     height:30,
-    opacity:0.1,
+    opacity:0.8,
     flexDirection:'row',
     justifyContent:'space-around',
     alignItems:'center'
   },
   tipItemContainer:{
-    width:60,
+    width:120,
     flexDirection:'row',
-    backgroundColor:'black',
+		alignItems:'center',
+		backgroundColor:'gray',
     height:30
-  }
-
+  },
+	titleContainer:{
+		flexDirection:'row',
+		padding:5,
+		alignItems:'center',
+		marginTop:10,
+		marginBottom:10,
+		marginLeft:10
+	},
+	priceContainer:{
+		marginLeft:10,
+		flexDirection:'row',
+		alignItems:'center'
+	},
+	picdocContainer:{
+		flexDirection:'row',
+		alignItems:'center',
+		height:30,
+		borderTopWidth:0.8,
+		borderBottomWidth:0.8,
+		marginLeft:10
+	}
 });
